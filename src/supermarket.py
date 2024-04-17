711877ebd02b0055a7e56e6b7836af18c9c9dc22
cookie-estyle <k.kurosawa@estyle-inc.jp>
2024-04-17 17:49:34 +0900

    インデックスを指定して金額を取り出す関数を作成

---
+++ src/supermarket.py
@@ -0,0 +1,13 @@
+import json
+
+with open("../item/item.json") as f:
+    item_list = json.load(f)
+
+# print(item_list[0].get("商品番号"))
+# print(item_list[0].get("商品名"))
+# print(item_list[0].get("金額"))
+
+def unit_price(item_num):
+    return item_list[item_num-1].get("金額")
+
+print(unit_price(6))
