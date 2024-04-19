ccea7dcd1e9ebdb2314d101cea9ca13afc92a2d6
molmolkky <y.fujimori@estyle-inc.jp>
2024-04-19 16:16:07 +0900

    お題2まで完了

--- src/supermarket.py
+++ src/supermarket.py
@@ -3,10 +3,18 @@ import json
 with open("../item/item.json") as f:
     item_list = json.load(f)
 
-def unit_price(item_num):
-    p = []
-    for i in item_num:
-        p.append(item_list.get(str(i)).get('金額'))
-    return sum(p)
+order = [[7, 5], [2, 11], [8, 8]]
 
-print(unit_price([9,4,6]))
+def item_price(item_num, amount):
+    return item_list.get(str(item_num)).get('金額')*amount
+
+def in_tax(total_price):
+    return total_price*1.08
+
+def unit_price(order):
+    p = []
+    for i in order:
+        p.append(item_price(*i))
+    return in_tax(sum(p))
+
+print(unit_price(order))
