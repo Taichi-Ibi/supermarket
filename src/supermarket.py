5fa35ec280a2c063a329784f737ea6ff0e633ea5
cookie-estyle <k.kurosawa@estyle-inc.jp>
2024-04-19 16:01:46 +0900

    補題２

--- src/supermarket.py
+++ src/supermarket.py
@@ -3,11 +3,10 @@ import json
 with open("../item/item.json") as f:
     item_list = json.load(f)
 
-# print(item_list[0].get("商品番号"))
-# print(item_list[0].get("商品名"))
-# print(item_list[0].get("金額"))
-
 def unit_price(item_num):
-    return item_list.get(str(item_num)).get('金額')
+    p = []
+    for i in item_num:
+        p.append(item_list.get(str(i)).get('金額'))
+    return sum(p)
 
-print(unit_price(6))
+print(unit_price([9,4,6]))
