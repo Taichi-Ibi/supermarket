12ff0826e00f5c46f41f8638de8719be0fea495f
molmolkky <y.fujimori@estyle-inc.jp>
2024-04-19 15:50:19 +0900

    補題1までできた

--- src/supermarket.py
+++ src/supermarket.py
@@ -8,6 +8,6 @@ with open("../item/item.json") as f:
 # print(item_list[0].get("金額"))
 
 def unit_price(item_num):
-    return item_list[item_num-1].get("金額")
+    return item_list.get(str(item_num)).get('金額')
 
 print(unit_price(6))
