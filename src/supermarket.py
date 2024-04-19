22c33cb55f7305d8a0a7766efb5a8fb533c8b03f
cookie-estyle <k.kurosawa@estyle-inc.jp>
2024-04-19 16:55:03 +0900

    お題3まで

--- src/supermarket.py
+++ src/supermarket.py
@@ -3,7 +3,7 @@ import json
 with open("../item/item.json") as f:
     item_list = json.load(f)
 
-order = [[7, 5], [2, 11], [8, 8]]
+order = [[7, 1], [1, 5], [6, 1]]
 
 def item_price(item_num, amount):
     return item_list.get(str(item_num)).get('金額')*amount
@@ -17,4 +17,24 @@ def unit_price(order):
         p.append(item_price(*i))
     return in_tax(sum(p))
 
-print(unit_price(order))
+def tabaco_price(order):
+    p = []
+    for i in order:
+        p.append(item_price(*i))
+    return sum(p)
+
+def total_price(order):
+    t = []
+    other = []
+
+    for n in order:
+        if n[0] == 6:
+            t.append(n)
+        elif n[0] == 7:
+            t.append(n)
+        else:
+            other.append(n)
+    
+    return tabaco_price(t) + unit_price(other)
+
+print(total_price(order))
